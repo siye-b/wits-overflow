3b08e75bff23ecf45ab883b5c941efb519dec89c
umair-b <1871892@students.wits.ac.za>
2021-04-02 20:28:26 +0200

    Update MainActivity.java
    Added regex for email and password validation.

--- app/src/main/java/com/example/witsly/MainActivity.java
+++ app/src/main/java/com/example/witsly/MainActivity.java
@@ -6,7 +6,29 @@ import android.os.Bundle;
 
 import com.example.witsly.databinding.ActivityMainBinding;
 
+import java.util.regex.Pattern;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final Pattern EMAIL_ADDRESS =
+            Pattern.compile(
+            "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" + //between 1 and 256 characters which contain alphanumeric symbols and certain characters
+                    "\\@" + //@ sign
+                    "(students.wits.ac.za|wits.ac.za)" //two Wits associated domains
+    );
+
+    private static final Pattern PASSWORD_PATTERN =
+            Pattern.compile("^" +
+                    "(?=.*[0-9])" +         //at least 1 digit
+                    "(?=.*[a-z])" +         //at least 1 lower case letter
+                    "(?=.*[A-Z])" +         //at least 1 upper case letter
+                    "(?=.*[a-zA-Z])" +      //any upper or lower case letter
+                    "(?=.*[@#$%^&+=!])" +    //at least 1 special character
+                    "(?=\\S+$)" +           //no white spaces
+                    ".{8,}" +               //at least 8 characters
+                    "$"
+            );
 
     public ActivityMainBinding binding;
 
