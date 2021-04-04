f58cadb3400db82c50937704a7b40ee0dec2bfaf
CarolineKhwerana <1886537@students.wits.ac.za>
2021-04-04 15:50:29 +0200

    added new test case

---
+++ app/src/androidTest/java/com/example/witsly/MainActivityTest.java
@@ -0,0 +1,20 @@
+package com.example.witsly;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenario = new ActivityScenarioRule<>(MainActivity.class);
+    
+    @Test
+    public void clickRegister_currentActivityIsMainActivity_currentActivityIsRegister(){
+        activityScenario.getScenario().onActivity(activity -> {
+            activity.binding.tvRegister.performClick();
+        });
+    }
+
+}
