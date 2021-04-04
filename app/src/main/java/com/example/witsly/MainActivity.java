76c81984ade58e8455011722667b8dcd6919dae8
CarolineKhwerana <1886537@students.wits.ac.za>
2021-04-04 13:31:21 +0200

    initial commit

--- app/src/main/java/com/example/witsly/MainActivity.java
+++ app/src/main/java/com/example/witsly/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.witsly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.witsly.databinding.ActivityMainBinding;
 
@@ -38,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+
 
     }
 
