a4ef88241cdf78562e6c034a285a7596cd000cbd
umair-b <1871892@students.wits.ac.za>
2021-04-05 19:56:58 +0200

    Update RegisterActivity.java

--- app/src/main/java/com/example/witsly/RegisterActivity.java
+++ app/src/main/java/com/example/witsly/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.witsly;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -68,6 +69,15 @@ public class RegisterActivity extends AppCompatActivity {
                 registerUser(name, surname, email, password1);
             }
 
+        });
+
+        binding.tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
         });
 
 
