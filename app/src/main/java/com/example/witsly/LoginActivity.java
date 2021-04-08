315925772ca43b3e208a3fffc5c6744641b5060f
user <1920451@students.wits.ac.za>
2021-04-08 21:51:48 +0200

    Keep User logged IN

--- app/src/main/java/com/example/witsly/LoginActivity.java
+++ app/src/main/java/com/example/witsly/LoginActivity.java
@@ -10,12 +10,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextInputLayout loginEmail, loginPassword;
     private FirebaseAuth mAuth;
     private TextView tv_register;
+    FirebaseUser mUser;
 
 
     @Override
@@ -28,12 +30,15 @@ public class LoginActivity extends AppCompatActivity {
 
 
         mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
         loginEmail = findViewById(R.id.til_email);
         loginPassword = findViewById(R.id.til_password);
         tv_register = findViewById(R.id.tv_register);
         Button loginButton = findViewById(R.id.btn_singin);
 
-
+        if (mUser != null) {
+            startActivity(new Intent(this,MainActivity.class));
+        }
         if (intent1 != null) {
             String str = intent1.getStringExtra("email");
             loginEmail.getEditText().setText(str);
