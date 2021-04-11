6700c16b4e55595e70102cb0a0c82c5c99a531e7
user <1920451@students.wits.ac.za>
2021-04-11 02:07:09 +0200

    Forgot password ,server side added

--- app/src/main/java/com/example/witsly/LoginActivity.java
+++ app/src/main/java/com/example/witsly/LoginActivity.java
@@ -7,10 +7,13 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.witsly.databinding.ForgotDialogBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -77,11 +80,20 @@ public class LoginActivity extends AppCompatActivity {
         builder.setView(binding.getRoot());
 
         builder.setPositiveButton("Next", (dialog, which) -> {
-            //Enter code for firebase here
-
 
             String email = binding.etForgotPW.getText().toString().trim();
-            Toast.makeText(LoginActivity.this, "Email is " + email, Toast.LENGTH_LONG).show();
+            mAuth.sendPasswordResetEmail(email).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    Toast.makeText(LoginActivity.this,"Password sent to your email: "+ email,Toast.LENGTH_LONG).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(LoginActivity.this,"ERROR ! Reset link is not sent ,"+ e.getMessage(),Toast.LENGTH_LONG).show();
+                }
+            });
+
             dialog.dismiss();
         });
 
