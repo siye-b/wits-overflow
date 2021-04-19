1fac5a31af41985234843ecf3fc618a8bd889ccd
umair-b <1871892@students.wits.ac.za>
2021-04-19 07:25:34 +0200

    Update HomeFragment.java

--- app/src/main/java/com/example/witsly/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/witsly/Fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.witsly.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,16 +8,27 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.example.witsly.PostActivity;
 import com.example.witsly.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomeFragment extends Fragment {
+
+    private FloatingActionButton mFAB;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        mFAB = view.findViewById(R.id.btn_new_post);
 
+        mFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), PostActivity.class));
+            }
+        });
         return view;
     }
 }
