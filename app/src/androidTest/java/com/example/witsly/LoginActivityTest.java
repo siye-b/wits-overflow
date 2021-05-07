effe17581723a277f00d3ce428a72b531fdf6caa
MOLOFP <1587653@students.wits.ac.za>
2021-05-07 11:47:32 +0200

    initial unit test for loginActivity

---
+++ app/src/androidTest/java/com/example/witsly/LoginActivityTest.java
@@ -0,0 +1,25 @@
+package com.example.witsly;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<LoginActivity> activityScenarioRule = new ActivityScenarioRule<>(LoginActivity.class);
+
+    @Test
+    public void loginUser_correctCredentials_loginSuccess(){
+        activityScenarioRule.getScenario().onActivity(activity -> {
+            activity.loginEmail.getEditText().setText("sfgdfgsd");
+            activity.loginPassword.getEditText().setText("sfdfdf");
+            activity.loginButton.performClick();
+        });
+    }
+
+
+}
