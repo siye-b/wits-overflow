f5436f7a7721dc644cab6e7adf29582ea12001e1
MOLOFP <1587653@students.wits.ac.za>
2021-05-09 22:50:29 +0200

    initial unit test for MainActivity

---
+++ app/src/androidTest/java/com/example/witsly/MainActivityTest.java
@@ -0,0 +1,34 @@
+package com.example.witsly;
+
+import android.view.MenuItem;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void onNavigationItemSelected() {
+
+        activityScenarioRule.getScenario().onActivity(activity -> {
+
+            MenuItem homeItem = activity.navigationView.getMenu().getItem(0);
+            activity.onNavigationItemSelected(homeItem);
+
+            MenuItem userItem = activity.navigationView.getMenu().getItem(1);
+            activity.onNavigationItemSelected(userItem);
+
+            MenuItem logoutItem = activity.navigationView.getMenu().getItem(2);
+            activity.onNavigationItemSelected(logoutItem);
+
+
+        });
+    }
+}
