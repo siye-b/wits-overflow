e08db52a8a4bcc8dc601fce840f1a429982a6dd9
MOLOFP <1587653@students.wits.ac.za>
2021-05-10 09:40:45 +0200

    initial unit test for registerActivity

--- app/src/androidTest/java/com/example/witsly/RegisterActivityTest.java
+++ app/src/androidTest/java/com/example/witsly/RegisterActivityTest.java
@@ -16,7 +16,7 @@ public class RegisterActivityTest {
     public void registerUser_correctCredentials_loginSuccess(){
         activityScenarioRule.getScenario().onActivity(activity -> {
             activity.regName.getEditText().setText("sfgdfgsd");
-            activity.regSurname.getEditText().setText("sfdfdffdf");
+            activity.regSurname.getEditText().setText("sfdfdf");
             activity.regEmail.getEditText().setText("sfdfdf");
             activity.regPasswordOne.getEditText().setText("sfdfdf");
             activity.regPasswordTwo.getEditText().setText("sfdfdf");
