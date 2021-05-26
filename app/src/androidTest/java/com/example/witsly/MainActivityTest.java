a4748e16baafec355c30e90ae690c5a72d3a3ce9
MOLOFP <1587653@students.wits.ac.za>
2021-05-26 16:08:07 +0200

    initial unit test for mainActivity

--- app/src/androidTest/java/com/example/witsly/MainActivityTest.java
+++ app/src/androidTest/java/com/example/witsly/MainActivityTest.java
@@ -9,25 +9,25 @@ import org.junit.Test;
 
 public class MainActivityTest {
 
-/*  @Rule
-  public ActivityScenarioRule<MainActivity> activityScenarioRule =
-      new ActivityScenarioRule<>(MainActivity.class);
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
 
-  @Test
-  public void onNavigationItemSelected() {
+    @Test
+    public void onNavigationItemSelected() {
 
-    activityScenarioRule
-        .getScenario()
-        .onActivity(
-            activity -> {
-              final MenuItem home_Item = activity.navigationView.getMenu().getItem(0);
-              activity.onNavigationItemSelected(home_Item);
+        activityScenarioRule
+                .getScenario()
+                .onActivity(
+                        activity -> {
+                            final MenuItem home_Item = activity.navigationView.getMenu().getItem(0);
+                            activity.onNavigationItemSelected(home_Item);
 
-              final MenuItem user_Item = activity.navigationView.getMenu().getItem(1);
-              activity.onNavigationItemSelected(user_Item);
+                            final MenuItem user_Item = activity.navigationView.getMenu().getItem(1);
+                            activity.onNavigationItemSelected(user_Item);
 
-              final MenuItem logout_Item = activity.navigationView.getMenu().getItem(2);
-              activity.onNavigationItemSelected(logout_Item);
-            });
-  }*/
+                            final MenuItem logout_Item = activity.navigationView.getMenu().getItem(2);
+                            activity.onNavigationItemSelected(logout_Item);
+                        });
+    }
 }
