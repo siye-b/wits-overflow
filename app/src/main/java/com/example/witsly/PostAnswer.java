4535daf08bbb454d7b8c8cd627ada7db18cb2b0e
umair-b <1871892@students.wits.ac.za>
2021-06-04 11:49:55 +0200

    Update PostAnswer.java

--- app/src/main/java/com/example/witsly/PostAnswer.java
+++ app/src/main/java/com/example/witsly/PostAnswer.java
@@ -7,9 +7,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 
+import com.example.witsly.Models.Comment;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
@@ -52,9 +54,22 @@ public class PostAnswer extends AppCompatActivity {
                 return true;
             case R.id.btn_post:
                 String bodyText = body.getText().toString().trim();
-
+                addComment(bodyText, mAuth.getUid(), " ", " ");
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void addComment(String body, String uid, String aid, String cid){
+        Comment comment = new Comment(body, uid, aid, cid);
+
+        firebaseActions.addComment(comment,
+                a->{
+                        if(a){
+                            Toast.makeText(this, "Comment added succesfully", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show();
+                        }
+                });
     }
 }
