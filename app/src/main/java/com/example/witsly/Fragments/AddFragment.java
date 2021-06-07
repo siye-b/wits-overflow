f16deb9cdbb790db2beff8ffc499954d3839298f
Ntwanano Rikhotso <sanele.rikhotso.99@gmail.com>
2021-06-07 15:24:33 +0200

    fixing build

--- app/src/main/java/com/example/witsly/Fragments/AddFragment.java
+++ app/src/main/java/com/example/witsly/Fragments/AddFragment.java
@@ -3,9 +3,7 @@ package com.example.witsly.Fragments;
 public class AddFragment {
   Integer x1, x2;
 
-  public AddFragment() {
-    ;
-  }
+  public AddFragment() {}
 
   public Integer add(Integer x1, Integer x2) {
     return x1 + x2;
