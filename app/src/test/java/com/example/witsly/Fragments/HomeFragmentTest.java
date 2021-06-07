fd94e16ba0b38aac9fc475f27b4536e83d3feb52
MOLOFP <1587653@students.wits.ac.za>
2021-06-07 08:31:31 +0200

    initial unit test for HomeFragmentTest

---
+++ app/src/test/java/com/example/witsly/Fragments/HomeFragmentTest.java
@@ -0,0 +1,16 @@
+package com.example.witsly.Fragments;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HomeFragmentTest {
+
+    @Test
+    public void onCreateView() {
+    }
+
+    @Test
+    public void onCreateOptionsMenu() {
+    }
+}
