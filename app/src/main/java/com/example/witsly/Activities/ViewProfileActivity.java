0366df0ab1f8c6b0d25c59f6cbd4365f44cc000e
umair-b <1871892@students.wits.ac.za>
2021-08-21 10:11:56 +0200

    Fix Build

--- app/src/main/java/com/example/witsly/Activities/ViewProfileActivity.java
+++ app/src/main/java/com/example/witsly/Activities/ViewProfileActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.witsly.R;
+
 public class ViewProfileActivity extends AppCompatActivity {
 
     @Override
