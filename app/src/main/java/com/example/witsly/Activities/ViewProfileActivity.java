dd57659be5d4662835095ec4731688d498779705
umair-b <1871892@students.wits.ac.za>
2021-08-21 09:57:41 +0200

    Added Activity

---
+++ app/src/main/java/com/example/witsly/Activities/ViewProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.witsly.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_profile);
+    }
+}
