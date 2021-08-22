061eab6530c8d2324dcf4451bec0b544f0b4c59e
MOLOFP <1587653@students.wits.ac.za>
2021-08-22 18:20:02 +0200

    Handle Login

--- app/src/main/java/com/example/witsly/Interfaces/HandleLogin.java
+++ app/src/main/java/com/example/witsly/Interfaces/HandleLogin.java
@@ -3,7 +3,7 @@ package com.example.witsly.Interfaces;
 public interface HandleLogin {
   // 1 wrong cred
   // 2 not registered
-  // 3 not verified
+  // 3 not verify
 
   void processAuth(Integer response, String msg);
 }
